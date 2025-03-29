e13d83c416a50dccd6d26571274fe9e01b01cc5f
Jensen-Jacob <jensenjacobgeorge@gmail.com>
2025-03-28 20:34:07 -0400

    first commit

---
+++ WeatherApp/CitiesViewController.swift
@@ -0,0 +1,21 @@
+//
+//  CitiesViewController.swift
+//  WeatherApp
+//
+//  Created by User on 2025-03-27.
+//
+
+import UIKit
+
+class CitiesViewController: UIViewController {
+    
+    var city: String? = nil;
+
+    override func viewDidLoad() {
+        super.viewDidLoad();
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+}
---
+++ WeatherApp/Weather.swift
@@ -0,0 +1,15 @@
+//
+//  Weather.swift
+//  WeatherApp
+//
+//  Created by User on 2025-03-28.
+//
+
+
+
+struct Weather: Decodable {
+    let location: Location;
+    let current: Current;
+}
+
+
