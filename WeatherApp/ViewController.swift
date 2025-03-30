8b9b9247c7e4bb3d145f7a04c16d42a35592118c
Jensen-Jacob <jensenjacobgeorge@gmail.com>
2025-03-29 23:15:51 -0400

    Fourth Commit: Added meaningful comments to ViewController.swift

--- WeatherApp/ViewController.swift
+++ WeatherApp/ViewController.swift
@@ -10,183 +10,199 @@ import CoreLocation;
 
 class ViewController: UIViewController, UITextFieldDelegate {
     
+    // API Key for WeatherAPI
     let API_KEY: String = "187719b8c7b14698b73213800252703";
+    
+    // Holds the current weather data
     var currentWeather: Weather?;
+    
+    // Array to store weather history (for displaying past weather searches)
     var weatherArray: Array<Weather>? = Array();
+    
+    // Location manager to get the user's current location
     var locationManager: CLLocationManager?;
+    
+    // Latitude and longitude string (used for searching by coordinates)
     var latlng: String = "";
+    
+    // Search query string (used for searching by city name)
     var searchQuery = "";
     
+    // Outlets for UI elements
     @IBOutlet weak var celsiusButton: UIButton!
-    
-    
     @IBOutlet weak var fahrenheitButton: UIButton!
-    
-    
     @IBOutlet weak var textFieldOutlet: UITextField!
-    
-    
     @IBOutlet weak var imageOutlet: UIImageView!
-    
     @IBOutlet weak var temperatureLabel: UILabel!
-    
     @IBOutlet weak var cityLabel: UILabel!
-    
     @IBOutlet weak var conditionLabel: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad();
+        
+        // Set the textFieldOutlet's delegate to this view controller
         textFieldOutlet.delegate = self;
+        
+        // Initialize the location manager
         locationManager = CLLocationManager();
         locationManager?.delegate = self;
+        
+        // Request permission to use location services
         locationManager?.requestWhenInUseAuthorization();
+        
+        // Request the user's current location
         if let locationMgr = locationManager {
             locationMgr.requestLocation();
         }
     }
     
-    
+    // Action when the "Navigator" button is pressed
     @IBAction func onNavigatorButtonPressed(_ sender: UIButton) {
-        searchLatLng();
+        searchLatLng(); // Search by latitude and longitude
     }
     
-    
+    // Action when the "Fahrenheit" button is pressed
     @IBAction func onFahrenheitButtonPressed(_ sender: UIButton) {
+        // Display temperature in Fahrenheit if available
         if let value = currentWeather?.current.heatindex_f {
             temperatureLabel.text = "\(value)";
         }
     }
     
-    
+    // Action when the "Celsius" button is pressed
     @IBAction func onCelsiusButtonPressed(_ sender: UIButton) {
+        // Display temperature in Celsius if available
         if let value = currentWeather?.current.heatindex_c {
             temperatureLabel.text = "\(value)";
         }
     }
     
+    // Updates the search query as the text field value changes
     func textFieldDidChangeSelection(_ textField: UITextField) {
         switch textField.tag {
         case 0:
             if let value = textField.text {
-                searchQuery = value;
+                searchQuery = value; // Store the search query
             }
-            
         default:
             print("nothing");
         }
     }
     
+    // Handles the return key press event on the text field (initiates search)
     func textFieldShouldReturn(_ textField: UITextField) -> Bool {
-        textField.resignFirstResponder()
-        onSearchButtonPressed(UIButton())
+        textField.resignFirstResponder() // Dismiss the keyboard
+        onSearchButtonPressed(UIButton()) // Initiate search by pressing the search button
         return true
     }
     
+    // Action when the "Cities" button is pressed
     @IBAction func onCitiesButtonPressed(_ sender: UIButton) {
+        // Segue to the cities view (for showing past searches)
         performSegue(withIdentifier: "homeToCitiesSegue", sender: self);
     }
     
+    // Action when the "Search" button is pressed
     @IBAction func onSearchButtonPressed(_ sender: UIButton) {
+        // Only search if the search query is not empty
         if (searchQuery != "") {
-            let url = getUrl(search: searchQuery);
+            let url = getUrl(search: searchQuery); // Get the search URL for the query
             let uRLSession = URLSession(configuration: .default);
             
-            //setup the datatask in the url session thread
+            // Setup a data task in the URL session thread to fetch weather data
             let dataTask = uRLSession.dataTask(with: url, completionHandler: requestCompletionHandler(data:response:error:));
             
-            //run the datatask
+            // Run the data task
             dataTask.resume();
+            
+            // Reset search query and text field after the search
             searchQuery = "";
             textFieldOutlet.text = "";
         }
     }
     
-    
+    // Searches for weather data using latitude and longitude
     func searchLatLng() {
-        let url = getUrl(search: self.latlng);
+        let url = getUrl(search: self.latlng); // Get the URL for the lat/lng search
         let uRLSession = URLSession(configuration: .default);
         
-        
-        //setup the datatask in the url session thread
+        // Setup a data task in the URL session thread to fetch weather data
         let dataTask = uRLSession.dataTask(with: url, completionHandler: requestCompletionHandler(data:response:error:));
 
-        //run the datatask
+        // Run the data task
         dataTask.resume();
     }
     
-    
+    // Prepares for segue to CitiesViewController to display weather history
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         if segue.identifier == "homeToCitiesSegue" {
             let destination = segue.destination as! CitiesViewController;
             if let array = weatherArray {
-                destination.weatherHistory = array;
+                destination.weatherHistory = array; // Pass the weather history to the next view
             }
         }
     }
     
+    // Returns the URL used to fetch weather data based on the search query
     func getUrl(search: String) -> URL {
-        //endpoint
         let baseUrl = "https://api.weatherapi.com/v1";
         let document = "/current.json";
-        let queryParam = "q=\(search)";
-        let keyParam = "key=187719b8c7b14698b73213800252703";
+        let queryParam = "q=\(search)"; // Search query parameter (city name or lat/lng)
+        let keyParam = "key=\(API_KEY)"; // API key parameter
         let endPoint = "\(baseUrl)\(document)?\(queryParam)&\(keyParam)";
         
-        //url object
+        // Return the constructed URL
         let url: URL? = URL(string: endPoint);
         return url!;
     }
     
-    
+    // Completion handler for the URL session data task to process the response
     func requestCompletionHandler(data: Data?, response: URLResponse?, error: Error?) {
-            
-            //filter out where error does not exist
-            if (error == nil) {
+        // Check for errors
+        if (error == nil) {
+            // Check if data exists in the response
+            if (data != nil) {
+                // Convert the data to a string for logging/debugging
+                let dataString: String? = String(data: data!, encoding: .utf8);
                 
-                //filter out where data has a value
-                if (data != nil) {
+                // Log the response data
+                if dataString != nil {
+                    print("\(dataString!)");
                     
-                    //stringify the data in the response
-                    let dataString: String? = String(data: data!, encoding: .utf8);
-                    
-                    //if stringify is successfully done
-                    if dataString != nil {
-                        print("\(dataString!)");
-                        
-                        let weather = parseJson(data: data!);
-                    }
-                    else {
-                        //print if data stringifying failed
-                        print("No data string");
-                    }
+                    // Parse the JSON data into a Weather object
+                    let weather = parseJson(data: data!);
+                } else {
+                    print("No data string");
                 }
-                else {
-                    //print if data is nil in the response
-                    print("No data received");
-                }
+            } else {
+                print("No data received");
             }
-            else {
-                //print if there is an error returned by the completion handler
-                print(error!);
-            }
+        } else {
+            print(error!); // Log any errors from the request
         }
+    }
     
-    //function that parses Data object into the full Decodable object
+    // Parses the received JSON data into a Weather object
     func parseJson(data: Data) -> Weather? {
         let decoder = JSONDecoder();
-        
         var weather: Weather?;
         
         do {
+            // Decode the JSON data into a Weather object
             weather = try decoder.decode(Weather.self, from: data);
+            
+            // Update the UI on the main thread
             DispatchQueue.main.async {
                 if let weatherObject = weather {
-                    self.weatherArray?.append(weatherObject);
-                    self.currentWeather = weatherObject;
-                    print("\(self.weatherArray?.count)");
+                    self.weatherArray?.append(weatherObject); // Add the new weather data to history
+                    self.currentWeather = weatherObject; // Set the current weather
+                    
+                    // Update the UI labels and image
                     self.temperatureLabel.text = "\(weatherObject.current.heatindex_c)";
                     self.conditionLabel.text = "\(weatherObject.current.condition.text)";
                     self.cityLabel.text = "\(weatherObject.location.name)";
+                    
+                    // Update the weather icon
                     let config = UIImage.SymbolConfiguration(paletteColors: [.systemBlue, .systemOrange]);
                     self.imageOutlet.preferredSymbolConfiguration = config;
                     self.imageOutlet.image = UIImage(systemName: "cloud.rain");
@@ -194,19 +210,17 @@ class ViewController: UIViewController, UITextFieldDelegate {
             }
         }
         catch {
-            print(error);
+            print(error); // Log any errors during JSON parsing
         }
         
         return weather;
     }
-
 }
 
-
-
-
+// CLLocationManagerDelegate extension for location services
 extension ViewController: CLLocationManagerDelegate {
     
+    // Handles changes in location authorization status
     func locationManagerDidChangeAuthorization(_ manager: CLLocationManager) {
         switch manager.authorizationStatus {
         case .notDetermined:
@@ -224,11 +238,14 @@ extension ViewController: CLLocationManagerDelegate {
         }
     }
     
+    // Updates location information when location is fetched
     func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
         if let location = locations.last {
             let lat = location.coordinate.latitude;
             let long = location.coordinate.longitude;
             print("Latitude \(lat) Longitude \(long)")
+            
+            // Update latlng and initiate search using coordinates
             DispatchQueue.main.async {
                 self.latlng = "\(lat),\(long)";
                 self.searchLatLng();
@@ -236,8 +253,8 @@ extension ViewController: CLLocationManagerDelegate {
         }
     }
     
+    // Handles location fetching errors
     func locationManager(_ manager: CLLocationManager, didFailWithError error: any Error) {
         print("Error fetching location \(error)");
     }
-    
 }
